cfedaa1ebad60961d186bbf70c4dd22048246156
noxiD30 <dicksonlow2000@hotmail.com>
2021-09-11 21:23:49 +0800

    addrack to db

--- app/src/main/java/my/edu/tarc/hotayi/WarehouseMapFragment.kt
+++ app/src/main/java/my/edu/tarc/hotayi/WarehouseMapFragment.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,12 +16,15 @@ import android.widget.*
 import androidx.fragment.app.Fragment
 import com.google.firebase.database.*
 import my.edu.tarc.hotayi.dataclass.Material
+import android.view.Gravity
+
+import android.widget.AdapterView
+import androidx.core.view.children
+import my.edu.tarc.hotayi.dataclass.Rack
 
 
 class WarehouseMapFragment : Fragment() {
-    var totalracksize = 1
     val materialList = arrayListOf<Material>()
-
 
 
     override fun onCreateView(
@@ -28,20 +32,94 @@ class WarehouseMapFragment : Fragment() {
         savedInstanceState: Bundle?
 
 
-
     ): View? {
         // Inflate the layout for this fragment
-        val view  =inflater.inflate(R.layout.fragment_warehouse_map, container, false)
-        val fl: FrameLayout = view.findViewById(R.id.frame_Layout)
-        getFirebaseData()
+        val view = inflater.inflate(R.layout.fragment_warehouse_map, container, false)
+        val popupview = inflater.inflate(R.layout.warehouse_popup_window, container, false)
+        val fl: FrameLayout = view.findViewById(R.id.frame_layout)
+
         fl.addView(view())
         //do a looping to set clickable
-        for(x in 1 until totalracksize){
-            val tv1 = view.findViewById<TextView>(x)
+        for (x in materialList) {
+            val tv1 = view.findViewById<TextView>(x.location)
             buttonEffect(tv1)
         }
+        //spinner
+        val spn = view.findViewById<Spinner>(R.id.spn_Admin)
 
+        spn.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long,
+            ) {
+                if(position!=0) {
+                    onSpinnerSelected(popupview, fl, position,spn)
+                }
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {}
+        }
         return view
+    }
+
+    private fun onSpinnerSelected(popupview:View, fl:FrameLayout, position:Int, spn:Spinner){
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        val btncancel = popupview.findViewById<Button>(R.id.btn_Cancel)
+        val btnconfirm = popupview.findViewById<Button>(R.id.btn_Confirm)
+        val params = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+        )
+        params.gravity = Gravity.CENTER
+
+
+        updateView(fl,spn,true)
+
+        btncancel.setOnClickListener {
+            updateView(fl,spn,false)
+        }
+        if(position == 1) {
+
+            btnconfirm.setOnClickListener{
+                val inputnum = popupview.findViewById<EditText>(R.id.input_num).text.toString().toInt()
+                val spnrow = popupview.findViewById<Spinner>(R.id.spn_Row).selectedItem.toString().toInt()
+                val spncol = popupview.findViewById<Spinner>(R.id.spn_Col).selectedItem.toString().toInt()
+                val myRef = FirebaseDatabase.getInstance().getReference("Rack")
+                myRef.get().addOnSuccessListener {
+                    var rackcount = it.childrenCount
+                    for(i in 1..inputnum) {
+                        var rackId = rackcount.toInt() + i
+                        var rack = Rack(rackId,spnrow,spncol)
+                        myRef.child(rackId.toString()).setValue(rack)
+                    }
+                }
+                updateView(fl,spn,false)
+                (popupview.parent as ViewGroup).removeView(popupview)
+            }
+        }
+        if(position == 2){
+
+        }
+
+
+
+        activity?.addContentView(popupview,params)
+    }
+
+    private fun updateView(fl: FrameLayout, spn: Spinner, bool: Boolean) {
+        if(!bool) {
+            for (x in fl.children) {
+                x.isEnabled = true
+            }
+        }else{
+
+            for (x in fl.children) {
+                x.isEnabled = false
+            }
+        }
+        spn.setSelection(0)
     }
 
     private fun view(): TableLayout {
@@ -51,6 +129,8 @@ class WarehouseMapFragment : Fragment() {
         )
         params.width = 1000
         params.height = 1200
+        params.setMargins(10,300,0,0)
+
 
         val table = TableLayout(activity)
         table.layoutParams = params
@@ -107,13 +187,13 @@ class WarehouseMapFragment : Fragment() {
         }
     }
 
-    private fun getFirebaseData(){
+    private fun getMaterialData(){
         val myRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("Material")
 
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for(snapshot in dataSnapshot.children){
-                    var material =Material(snapshot.child("location").toString().toInt(),snapshot.child("id").toString())
+                    var material =Material(snapshot.child("location").value.toString().toInt(),snapshot.child("id").value.toString())
                     materialList.add(material)
                 }
                 //Log.w(ContentValues.TAG, materialList[1].)
@@ -123,8 +203,7 @@ class WarehouseMapFragment : Fragment() {
                 Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
             }
         })
-
-
     }
+
 }
 
---
+++ app/src/main/java/my/edu/tarc/hotayi/dataclass/Rack.kt
@@ -0,0 +1,7 @@
+package my.edu.tarc.hotayi.dataclass
+
+data class Rack(
+    val Id:Int,
+    val row:Int,
+    val col:Int
+)
